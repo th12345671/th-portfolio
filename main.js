5fba7de63e97af26d5e6c2d944ac1cf6f44be677
th1234561 <wksk121234@naver.com>
2022-09-21 21:05:29 +0900

    add my language animation

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-function revealMyInfo(myInfo, className) {
+function revealMyInfo(myInfo, className, color, animationName) {
   let htmlElement = document.querySelector(className);
 
   htmlElement.innerHTML = myInfo;
   htmlElement.style.color = color;
+
+  htmlElement.style.animationDuration = "1s";
+
+  if (animationName === "bgFadeOut") {
+    htmlElement.style.animationName = animationName;
+  } else if (animationName === "slideIn") {
+    htmlElement.style.animationName = animationName;
+  } else if (animationName === "heartBeat") {
+    htmlElement.style.animationName = animationName;
+  }
 }
